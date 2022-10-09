afef99e4796e667588ac91b98fc13128b4de70e6
Thant Ko Zaw <thantmkou10@gmail.com>
2022-10-09 10:37:13 +0630

    Add sign up logic

--- src/features/auth/SignUpForm.tsx
+++ src/features/auth/SignUpForm.tsx
@@ -1,8 +1,19 @@
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-import { ISignUpCreds, useAuth } from './AuthProvider';
-import { Button } from '@chakra-ui/react';
+import {
+  Button,
+  Container,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import { useSignUp } from './api/useSignUp';
+import { ISignUpCreds } from './types';
 
 const schema = yup.object({
   email: yup.string().email('Must be a valid email').required(),
@@ -24,26 +35,50 @@ const SignUpForm = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<ISignUpCreds>({
     resolver: yupResolver(schema),
   });
-  const { signUp } = useAuth();
+
+  const mutation = useSignUp();
+  const navigate = useNavigate();
 
   const onSubmit = async (data: ISignUpCreds) => {
-    try {
-      await signUp(data);
-    } catch (err: unknown) {
-      if (err instanceof Error) {
-        console.log(err.message);
-      }
-    }
+    await mutation.mutateAsync(data);
+    navigate('/login');
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
-      <Button type="submit">Sign Up</Button>
-    </form>
+    <Container>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+        {mutation.isError && <Text>{(mutation.error as Error).message}</Text>}
+        <VStack maxW="50rem" gap={4} alignItems="start">
+          <FormControl>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input {...register('email')} id="email" type="email" />
+            <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input {...register('password')} id="password" type="password" />
+            <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="firstName">First Name</FormLabel>
+            <Input {...register('firstName')} id="firstName" type="text" />
+            <FormErrorMessage>{errors.firstName?.message}</FormErrorMessage>
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="lastName">Last Name</FormLabel>
+            <Input {...register('lastName')} id="lastName" type="text" />
+            <FormErrorMessage>{errors.lastName?.message}</FormErrorMessage>
+          </FormControl>
+          <Button type="submit" isLoading={isSubmitting}>
+            Sign Up
+          </Button>
+        </VStack>
+      </form>
+    </Container>
   );
 };
 
---
+++ src/features/auth/api/useSignUp.ts
@@ -0,0 +1,23 @@
+import { useMutation } from '@tanstack/react-query';
+import { ISignUpCreds } from '../types';
+
+const signUp = async (signUpCreds: ISignUpCreds) => {
+  const res = await fetch('/api/sign-up', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(signUpCreds),
+  });
+
+  if (!res.ok) {
+    const data = await res.json();
+    throw new Error(data.message as string);
+  }
+};
+
+export const useSignUp = () => {
+  return useMutation((data: ISignUpCreds) => {
+    return signUp(data);
+  });
+};
