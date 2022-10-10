8d3951689674960ec39ab340cb111fc3cbc0b105
Thant Ko Zaw <thantmkou10@gmail.com>
2022-10-10 12:54:35 +0630

    Add filtering options

--- src/features/posts/PostsContainer.tsx
+++ src/features/posts/PostsContainer.tsx
@@ -1,13 +1,50 @@
-import { Box, Heading, Skeleton, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Heading,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Skeleton,
+  VStack,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { BiChevronDown } from 'react-icons/bi';
 import { usePosts } from './api/usePosts';
 import PostCard from './PostCard';
 
 const PostsContainer = () => {
-  const { data: posts, isFetching } = usePosts();
+  const {
+    data: posts,
+    isFetching,
+    setFieldNameFilters,
+    setSortOrderFilters,
+  } = usePosts();
 
   return (
     <Box>
       <Heading as="h2">Posts</Heading>
+      <Menu>
+        <MenuButton
+          as={Button}
+          rightIcon={<BiChevronDown />}
+          variant="outline"
+          colorScheme="blue"
+          size="sm"
+          mt={4}
+        >
+          Sort By
+        </MenuButton>
+        <MenuList>
+          <MenuItem onClick={() => setFieldNameFilters('createdAt')}>
+            Latest Posts
+          </MenuItem>
+          <MenuItem onClick={() => setFieldNameFilters('likes')}>
+            Top Posts
+          </MenuItem>
+        </MenuList>
+      </Menu>
       <VStack mt={4} spacing="1.5rem">
         {!posts && isFetching && (
           <>
--- src/features/posts/api/usePosts.ts
+++ src/features/posts/api/usePosts.ts
@@ -1,14 +1,38 @@
 import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { TFieldNameFilters, TSortOrder } from '../../type';
 import { IPost } from '../types';
 
-const getPosts = async () => {
-  const res = await fetch('/api/posts');
+interface IPostFilters {
+  fieldName?: TFieldNameFilters;
+  sortOrder?: TSortOrder;
+}
 
+const getPosts = async ({
+  fieldName,
+  sortOrder,
+}: IPostFilters): Promise<IPost[]> => {
+  const res = await fetch(
+    `/api/posts?fieldName=${fieldName}&sortOrder=${sortOrder}`
+  );
   const data = await res.json();
 
   return data.data as IPost[];
 };
 
 export const usePosts = () => {
-  return useQuery(['posts'], getPosts);
+  const [filters, setFilters] = useState<IPostFilters>({
+    fieldName: 'createdAt',
+    sortOrder: 'desc',
+  });
+
+  const setFieldNameFilters = (fieldName: TFieldNameFilters) =>
+    setFilters((prev) => ({ ...prev, fieldName }));
+
+  const setSortOrderFilters = (sortOrder: TSortOrder) =>
+    setFilters((prev) => ({ ...prev, sortOrder }));
+
+  const query = useQuery(['posts', filters], () => getPosts(filters));
+
+  return { ...query, setFieldNameFilters, setSortOrderFilters };
 };
